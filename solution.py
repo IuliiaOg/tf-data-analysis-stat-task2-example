70f3c983cae820d548f765dfe81e27fab8c8c40a
IuliiaOg <129860977+IuliiaOg@users.noreply.github.com>
2023-04-08 00:00:31 +0300

    Update solution.py
    1

--- solution.py
+++ solution.py
@@ -9,5 +9,5 @@ chat_id = 501141319
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p 
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return np.sqrt(0.47)*scale * norm.ppf(1 - alpha / 2), \
-           np.sqrt(0.47)*scale * norm.ppf(alpha / 2)
+    return scale * norm.ppf(1 - alpha / 2)/np.sqrt(47), \
+           scale * norm.ppf(alpha / 2)/np.sqrt(47)
